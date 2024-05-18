ec63555fb13f062b55fd32b7f65be6db3b73aae0
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-05-18 17:48:27 +0000

    Task 2 completed

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,4 @@
+// Task 1
 function processArray(number){
     let arr = []
 
@@ -15,3 +16,32 @@ function processArray(number){
 
 // Test the code
 console.log(processArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+
+
+// Task 2
+function formatArrayStrings(string, number) {
+    let stringArr = []
+
+    for (let i = 0; i < string.length; i++) {
+        let str = string[i]
+        let num = number[i]
+
+        if (num % 2 === 0) {
+            stringArr[stringArr.length] = str.toUpperCase()
+        } else {
+            stringArr[stringArr.length] = str.toLowerCase()
+        }
+        
+    }
+
+    return stringArr
+}
+// Test the code
+
+console.log(formatArrayStrings(["hello", "world", "how", "are", "you"], processArray([1, 2, 3, 4, 5])))
+
+module.exports = {
+    processArray,
+    formatArrayStrings
+};
